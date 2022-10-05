5e8acb93ccb7a882be0f47eabdae688492c16f7f
TristanBourbigot <tristanbourbigot@gmail.com>
2022-10-05 21:18:03 +0200

    update main

--- sport_track_web_app_php/views/main.php
+++ sport_track_web_app_php/views/main.php
@@ -1,9 +1,5 @@
-<?php include __ROOT__."/views/header.html"; ?>
-<h1> Main page</h1>
-<a href="/connect">Click here to display the connection form.</a>
-<br>
-<a href="/apropos">Click here to display the author.</a>
-<br>
-<a href="/user_add">Click here to create an acounte.</a>
-            
-<?php include __ROOT__."/views/footer.html"; ?>
+<?php 
+include __ROOT__."/views/header.html"; 
+include __ROOT__."/views/main.html";
+include __ROOT__."/views/footer.html"; 
+?>
