93aecd6c0a859ec80f7828a04446df2c76b8647c
TristanBourbigot <tristanbourbigot@gmail.com>
2022-10-05 16:50:23 +0200

    ratio se puch

--- js/user_DAO.js
+++ js/user_DAO.js
@@ -4,28 +4,36 @@ var UserDAO = function(){
     this.insert = function(values, callback){
         var sql = "insert into User(name, first_name, birthdate, gender, height, weight, email, password) values (?,?,?,?,?,?,?,?)";
         db.run(sql, values, callback);
+
     };
 
     this.update = function(key, values, callback){
         var sql = "UPDATE User SET name=?, first_name=?, birthdate=?, gender=?, height=?, weight=?, email=?, password=? WHERE id=?";
         values.push(key);
         db.run(sql, values, callback);
+        db.close();
+        return callback;
     };
 
     this.delete = function(key, callback){
         var sql = "DELETE FROM User WHERE id=?";
         db.run(sql, key, callback);
-
+        db.close();
+        return callback;
     };
 
     this.findAll = function(callback){
         var sql = "SELECT * FROM User ORDER BY id";
         db.run(sql, callback);
+        db.close();
+        return callback;
     };
 
     this.findByKey = function(key, callback){
         var sql = "SELECT * FROM User WHERE id=?";
-        var res = db.run(sql, key, callback);
+        db.run(sql, key, callback);
+        db.close();
+        return callback;
     };
 };
 var dao = new UserDAO();
