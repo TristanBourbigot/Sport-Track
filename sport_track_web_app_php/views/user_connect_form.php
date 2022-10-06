d0439c0955b8b98401050f0481e48424209ba934
TristanBourbigot <tristanbourbigot@gmail.com>
2022-10-06 18:08:07 +0200

    css connect

--- sport_track_web_app_php/views/user_connect_form.php
+++ sport_track_web_app_php/views/user_connect_form.php
@@ -1,12 +1,5 @@
-<?php include __ROOT__."/views/header.html"; ?>
-
-<h3>Connection</h3>
-<form action="/connect" method="post">
-  <label>Email :</label><br>
-  <input type="text" name="email" required><br>
-  <label>Mot de passe</label> :<br>
-  <input type="password" name="mdp" required><br><br>
-  <input type="submit" value="se connecté"><br>
-</form>
-            
-<?php include __ROOT__."/views/footer.html"; ?>
+<?php 
+include __ROOT__."/views/header.html"; 
+include __ROOT__."/views/user_connect_form.html";
+include __ROOT__."/views/footer.html"; 
+?>
