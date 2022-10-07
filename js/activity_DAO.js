db2baa21499200c4bf9802320787a029373f396f
RPZDesMicroOndes <divin.poo@gmail.com>
2022-10-07 10:52:03 +0200

    sqliteConnection

---
+++ js/activity_DAO.js
@@ -0,0 +1,44 @@
+var db = require('./sport-track-db/sqlite_connection');
+var usr = require('./user_DAO');
+var ActivityDAO = function(){
+
+    this.insert = function(values, callback){
+
+        var sql = "insert into Activity(name, description, date, distance, duration, user_id) values (?,?,?,?,?,?)";
+        db.run(sql, values, callback);
+        db.close();
+        return callback;
+
+    }
+
+    this.update = function(key, values, callback){
+
+        var sql = "UPDATE Activity SET name=?, description=?, date=?, distance=?, duration=?, user_id=? WHERE id=?";
+        values.push(key);
+        db.run(sql, values, callback);
+        db.close();
+        return callback;
+
+    }
+
+    this.delete = function(key, callback){
+            
+        var sql = "DELETE FROM Activity WHERE id=?";
+        db.run(sql, key, callback);
+        db.close();
+        return callback;
+
+    }
+    
+    this.findAll = function(callback){
+            
+        var sql = "SELECT * FROM Activity ORDER BY id";
+        db.run(sql, callback);
+        db.close();
+        return callback;
+
+    }
+
+}
+var dao = new ActivityDAO();
+module.exports = dao;
--- js/sport-track-db/sport_track_db_test.js
+++ js/sport-track-db/sport_track_db_test.js
@@ -1,3 +1,2 @@
 var user_dao = require('./sport_track_db').user_DAO;
-var db = require('./sport_track_db').db_connection;
-
+var db = require('./sport_track_db').db;
