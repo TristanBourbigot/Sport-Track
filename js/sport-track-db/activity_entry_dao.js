128e22e1d2ca371022841ce4412195e3de8313d1
TristanBourbigot <tristanbourbigot@gmail.com>
2022-10-07 17:16:30 +0200

    Add activity entry dao

--- js/sport-track-db/activity_entry_dao.js
+++ js/sport-track-db/activity_entry_dao.js
@@ -1,31 +1,31 @@
 var db = require('./sqlite_connection');
-var UserDAO = function(){
+var ActivityEntryDAO  = function(){
 
     this.insert = function(values, callback){
-        var sql = "insert into Data(theActivity, hour,cardioFrequency,lattitude,longitude,altitude) values (?,?,?,?,?,?)";
+        var sql = "insert into User(name, first_name, birthdate, gender, height, weight, email, password) values (?,?,?,?,?,?,?,?)";
         db.run(sql, values, callback);
     };
 
     this.update = function(key, values, callback){
-        var sql = "UPDATE User SET name=?, first_name=?, birthdate=?, gender=?, height=?, weight=?, email=?, password=? WHERE id=?";
+        var sql = "UPDATE Data SET theActivity=?, hour=?, cardioFrequency=?, lattitude=?, longitude=?, altitude=? WHERE idDATA=?";
         values.push(key);
         db.run(sql, values, callback);
     };
 
     this.delete = function(key, callback){
-        var sql = "DELETE FROM Data WHERE idData = ?";
+        var sql = "DELETE FROM Data WHERE idData=?";
         db.run(sql, key, callback);
     };
 
     this.findAll = function(callback){
-        var sql = "SELECT * FROM User ORDER BY id";
+        var sql = "select * from Data order by idData";
         db.all(sql,[], callback);
     };
 
     this.findByKey = function(key, callback){
-        var sql = "SELECT * FROM User WHERE id=?";
+        var sql = "SELECT * FROM Data WHERE idData=?";
         db.all(sql, key, callback);
     };
 };
-var dao = new UserDAO();
+var dao = new ActivityEntryDAO();
 module.exports = dao;
