7c3291c03beb80aa02943c0c820eecfe0d229c2e
TristanBourbigot <tristanbourbigot@gmail.com>
2022-10-21 03:35:00 +0200

    modif upload

--- js/express-webApp/routes/upload.js
+++ js/express-webApp/routes/upload.js
@@ -1,65 +1,57 @@
-var express = require('express');
-var fileUpload = require('express-fileupload');
+var express = require("express");
 var router = express.Router();
+var fileUpload = require('express-fileupload');
 var user_dao = require('sport_track_db').user_dao;
 var activity_dao = require('sport_track_db').activity_dao;
 var activity_entry_dao = require('sport_track_db').activity_entry_dao;
 
 router.use(fileUpload({}));
 
-router.get('/', function(req, res, next) {
-    res.render('upload', { title: 'Upload' });
+router.get('/', function(req, res) {
+    if(req.session.email) {
+        res.render('upload', { title: 'Upload' });
+    } else {
+        res.redirect('/connect');
+    }
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function(req, res) {
+    var mailUser = req.session.email;
 
-    if(req.session.email){
+    user_dao.findByEmail(mailUser, function(err, rows) {
 
-        var mailUser = req.session.email;
+        if(err != null){
 
-        user_dao.findByEmail(mailUser, function(err, rows) {
+            console.log("ERROR= " +err);
 
-            if(err != null){
+        }else {
 
-                console.log("ERROR= " +err);
+            var idUser = rows[0].id;
 
-            }else {
-
-                var idUser = rows[0].id;
-
-            }
-
-        });
-
-        try {
-
-            if(!req.files) {
-
-                console.log("No files were uploaded.");
-
-
-
-            } else {
-
-                //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
-                let activities = req.files.data;
-
-                console.log(activities);
-            
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(500).send(err);
         }
 
-    
+    });
 
-    }else{
+    try {
 
-        res.redirect('/connect');
+        if(!req.files) {
 
+            console.log("No files were uploaded.");
+
+
+
+        } else {
+
+            //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+            let activities = req.files.data;
+
+            console.log(activities);
+        
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
     }
-
 });
 
 module.exports = router;
